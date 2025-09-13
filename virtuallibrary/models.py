69c487f153c9c496ad9f6f54d6b0d6caf11760c5
Kauê Loreno <kaueusa2@gmail.com>
2025-09-13 12:41:48 -0300

    Adicionando tags aos livros

--- virtuallibrary/models.py
+++ virtuallibrary/models.py
@@ -1,15 +1,19 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
  def get_queryset(self):
      return (super().get_queryset().filter(status=Book.Status.PUBLISHED))
+ 
 class Book(models.Model):
+    tags = TaggableManager()
     class Status(models.TextChoices):
         DRAFT = 'DF','Draft'
         PUBLISHED = 'PB','Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -17,8 +21,8 @@ class Book(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='book_written',
-
     )
+
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
@@ -27,12 +31,14 @@ class Book(models.Model):
         choices = Status,
         default=Status.DRAFT    
     )
+
     objects = models.Manager()
     published = PublishedManager()
-class Meta:
+
+    class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields=['-publish']),
-        ]
-        def __str__(self):
-            return self.title
+                models.Index(fields=['-publish']),
+            ]
+    def __str__(self):
+        return self.title
--- virtuallibrary/views.py
+++ virtuallibrary/views.py
@@ -1,13 +1,32 @@
+from taggit.models import Tag
 from django.shortcuts import render
-from .models import Book
 from django.shortcuts import get_object_or_404, render
-# Create your views here.
-def book_list(request):
-    books = Book.published.all()
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
+from .models import Book
+
+def book_list(request,tag_slug=None):
+    book_list = Book.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        book_list = book_list.filter(tag_in=[tag])
+
+    paginator = Paginator(book_list,3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        books = paginator.page(page_number)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+        
     return render(
         request,
         'virtuallibrary/book/list.html',
-        {'books': books}
+        {'books': books,
+         'tag': tag}
     )
 def book_detail(request,id):
     book = get_object_or_404(Book,id=id,status=Book.Status.PUBLISHED)
