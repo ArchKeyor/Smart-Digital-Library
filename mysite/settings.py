a9081c81b32c4647f8b07e92b4520c2c567ed3c9
Pedro Dias <dias.contatodev@gmail.com>
2025-09-17 16:22:20 -0300

    Adicionando a página de usuário já com as funcionalidades django

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -130,6 +129,10 @@ LOGIN_REDIRECT_URL = '/virtuallibrary/home/'   # substitua pela página inicial 
 LOGOUT_REDIRECT_URL = '/'       # volta para a tela de login após logout
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'virtuallibrary' / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
